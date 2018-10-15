03ba25ee40d71d010dd106ceefce2dd70f966842
jasminehatcher <jazziacollege@gmail.com>
2018-10-15 12:17:59 -0400

    use $a0 to print first half of student ID (0285)

--- studentinfo.asm
+++ studentinfo.asm
@@ -9,3 +9,11 @@
     la $a0, 64         #@
 	syscall
 	sub $v0, 10        # 1 (11 - 10 = 1)
+    sub $a0, 64        # 0 (64 - 64 = 0)
+    syscall
+    addi $a0, 2        # 2 (0 + 2 = 2)
+    syscall
+    addi $a0, 6        # 8 (2 + 6 = 8)
+    syscall
+    sub $a0, 3         # 5 (8 - 3 = 5)
+	syscall
