4b480e55fe789daae10c3ec6826fc862004aaf95
jasminehatcher <jazziacollege@gmail.com>
2018-10-18 16:10:43 -0400

    print 'H', 'a', 't', 'c' using $a0 and ascii codes

--- studentinfo.asm
+++ studentinfo.asm
@@ -28,3 +28,11 @@
 	addi $v0, 10       # 11 (1 + 10 = 11)
     addi $a0, 5        # line break (5 + 5 = 10)
     syscall
+	addi $a0, 62       # H (10 + 62 = 72) 
+    syscall
+    addi $a0, 25       # a (72 + 25 = 97)
+    syscall
+    addi $a0, 19       # t (97 + 19 = 116)
+    syscall
+    sub $a0, 17        # c (116 - 17 = 99)
+	syscall
